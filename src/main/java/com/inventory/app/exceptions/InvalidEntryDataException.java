0c72eeda38ca411438b941d29efcf71d8e2569e1
Francisco Amado [1211763] <1211763@isep.ipp.pt>
2022-12-31 20:20:50 +0000

    Created and implemented custom exceptions

--- src/main/java/com/inventory/app/exceptions/InvalidEntryDataException.java
+++ src/main/java/com/inventory/app/exceptions/InvalidEntryDataException.java
@@ -2,7 +2,7 @@ package com.inventory.app.exceptions;
 
 public class InvalidEntryDataException extends RuntimeException{
 
-    public InvalidEntryDataException (String errorMessage) {
+    public InvalidEntryDataException(String errorMessage) {
         super(errorMessage);
     }
 }
