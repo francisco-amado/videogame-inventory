906ff54e72021d8d793e9b615bee50b9850621a1
Francisco Amado [1211763] <1211763@isep.ipp.pt>
2023-05-15 22:48:31 +0100

    Created webapp; added login and register pages

--- src/main/java/com/inventory/app/dto/CreateOwnerDTO.java
+++ src/main/java/com/inventory/app/dto/CreateOwnerDTO.java
@@ -2,13 +2,17 @@ package com.inventory.app.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
-@Component
-public class CreateOwnerDTO {
+import java.io.Serializable;
+
+public class CreateOwnerDTO implements Serializable {
+
+    private static final long serialVersionUID = 10L;
 
     private @Getter @Setter String userName;
     private @Getter @Setter String email;
     private @Getter @Setter String password;
     private @Getter @Setter String confirmPassword;
+
+    public CreateOwnerDTO() {}
 }
